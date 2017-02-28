7b7fc252ea40052a841bd76d3339eafdf0d91d6b
Kurt Maurer <kurtrm@gmail.com>
2017-02-27 17:06:58 -0800

    Added the currentPlayer variable and fixed margins on link buttons.

--- results.js
+++ results.js
@@ -2,21 +2,19 @@
 
 //TEST ARRAY: var results = ['Kurt', 75, false];
 
-// var results = JSON.parse(localStorage./*savedPlayerData*/);
-
-
-//results will contain [name, score, boolean (win = true, lose = false)]
+var currentPlayer = JSON.parse(localStorage.currentPlayer);
 
 var winMessage = 'Great job. You have won the game!';
 var lossMessage = 'Oh no, you lost. Time is screwed up.';
 
 var headerEl = document.getElementById('results');
 
-if (/* results[index].boolean */ === true) {
+//If the player wins, the Player.score attribute is set to true. It is false otherwise.
+if (currentPlayer.win === true) {
   headerEl.innerHTML = winMessage;
 } else {
   headerEl.innerHTML = lossMessage;
 }
 
 var scoreEl = document.getElementById('score-number');
-scoreEl.innerHTML = 'Score: ' + /*results[index].score*/;
+scoreEl.innerHTML = 'Score: ' + currentPlayer.score;
