419a4267cb972e2440ad290a0878e381b2549c0c
Danny McGill <dannymcgill@hotmail.com>
2017-02-27 18:04:14 -0800

    Started displaying questions.

---
+++ combat.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// Selects a random question.
+// function selectQuestion(questionType){
+//   if(questionType === 'attack'){
+//     var questionIndex = Math.floor(Math.random() * attackQuestions.length);
+//     return attackQuestions[questionIndex];
+//   }else if(questionType === 'defense'){
+//     //
+//   }else{
+//     //
+//   }
+// }
+
+var radioButtonOne = document.getElementById('answer-choice-1-radio-button');
+var radioButtonTwo = document.getElementById('answer-choice-2-radio-button');
+var radioButtonThree = document.getElementById('answer-choice-3-radio-button');
+var radioButtonFour = document.getElementById('answer-choice-4-radio-button');
+
+function handleSubmit(event){
+  //
+}
