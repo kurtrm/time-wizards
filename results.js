b493f9aa7f6dec75c5112b0d39fac29888d77f79
Kurt Maurer <kurtrm@gmail.com>
2017-03-01 11:42:47 -0800

    Commented the shell functionality of the stretch goal.

--- results.js
+++ results.js
@@ -2,10 +2,18 @@
 
 //TEST ARRAY: var results = ['Kurt', 75, false];
 
-localStorage.recordedPlayers += localStorage.currentPlayer;
-var currentPlayer = JSON.parse(localStorage.currentPlayer);
-recordedPlayers += currentPlayer;
-JSON.stringify(localStorage.recordedPlayers);
+//Stretch goal: display high scores of other players stored in local storage.
+
+//If there isn't an array called recordedPlayers, make an empty array in local storage.
+//Append the current player to the end of the recordedPlayers array.
+//If recordedPlayers.length < 5, display all of the available results.
+//Else if there's more than 5, display only the top 5 results.
+//Display them from highest score to lowest score.
+  // localStorage.recordedPlayers += localStorage.currentPlayer;
+  // recordedPlayers += currentPlayer;
+  // JSON.stringify(localStorage.recordedPlayers);
+
+var currentPlayer = JSON.parse(localStorage.getItem('player'));
 
 var winMessage = 'The robot is reassembled, and time has returned back to normal. Great job!';
 var lossMessage = 'You couldn\'t put the robot back together, and time is screwed up forever. Lucky for us, you can try again!';
