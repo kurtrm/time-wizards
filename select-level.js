3eb3b8e4489fb5530a035db69c49f1207d8cd11b
galang pham <phamgalang@gmail.com>
2017-03-01 13:55:22 -0800

    fixed local storage

--- select-level.js
+++ select-level.js
@@ -1,11 +1,26 @@
 'use-strict';
 
-//This section adds text data for the location
-
 // This section adds text data to historical figure
 var historicalFigureIntroEl = document.getElementById('historical-text');
 historicalFigureIntroEl.textContent = (einstein.intro);
 var historicalFigureImageEl = document.getElementById('historical-image');
 historicalFigureImageEl.setAttribute('src', einstein.image[0]);
 
-//This section adds text data for the enemy
+var historicalFigure = einstein;
+var scene = japan;
+
+function saveToLocalStorage () {
+  localStorage.setItem('historicalFigure', JSON.stringify(historicalFigure));
+  localStorage.setItem('scene', JSON.stringify(scene));
+  console.log('Saved to local storage');
+}
+
+function loadFromLocalStorage() {
+  player = JSON.parse(localStorage.getItem('player'));
+  scene = JSON.parse(localStorage.getItem('scene'));
+  historicalFigure = JSON.parse(localStorage.getItem('historicalFigure'));
+  console.log('Loaded from local storage');
+}
+
+saveToLocalStorage();
+loadFromLocalStorage();
