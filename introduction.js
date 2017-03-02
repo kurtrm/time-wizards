9fc549a51fd891bc47f9b231dddc396059c627e3
Danny McGill <dannymcgill@hotmail.com>
2017-03-02 15:43:54 -0800

    Robot now addresses you by your name.

--- introduction.js
+++ introduction.js
@@ -1,5 +1,16 @@
 'use strict';
 
+// Get the player's name for the introduction.
+var playerName;
+function loadLocalStorage(){
+  playerName = JSON.parse(localStorage.getItem('player')).name;
+}
+loadLocalStorage();
+
+// The element that the introduction text goes.
+var robotIntroductionEl = document.getElementById('robot-introduction');
+robotIntroductionEl.textContent = 'Hello ' + playerName + '! ' + 'A time anomaly has occured. There is a fracture in space time and you must fix this cosmic conundrum. My name is generic robot and a discrepancy in my subatomic-unilateral-colliding-konimeter has ruptured the delicate balance of space time. Time warp holes have been created and parts of my SUCK have been scattered. You must travel through these warp holes and retrieve my missing parts and fix my SUCK. Adventurer beware, these warp holes have an unknown destination and time period, so you must traverse carefully. The fate of space-time is in your hands, good luck!';
+
 //this block hides the div containers until user clicks on buttons
 var topBox = document.getElementById('button-one');
 var hideCenterBox = document.getElementById('center-box');
